4161a70e8573c69c36737f2db5ccfbcb2da4ef5f
Sven Gregori <sven@craplab.fi>
2025-10-17 00:48:47 +0300

    Initial commit, just authentication to API

---
+++ README.md
@@ -0,0 +1,14 @@
+# Spotify Mix Unfcker
+
+I'm a big fan of Spotify's generated daily and genre-specific mixes in general.
+They're great to just have a nice list of music going without thinking too much what to play next, and I've discovered plenty of great new-to-me songs through them.
+
+That said, I've come to absolutely loathe their inconsistency and disruption in the list handling.
+
+One too many times I had a list I enjoyed replaced the next day with a near-identical duplicate of another, already existing list.
+And one too many times I had a horribly timed refreshing of a list mess up the whole vibe of it.
+Obviously, after repeat-listening a certain song or band for the last hours, it must clearly mean I've had enough of it, so let's make sure it won't be on the refreshed list anymore.  @#%$@&!
+
+Copying manually some songs to a dedicated playlist is an option, sure, but I don't really want to make it a chore to enjoy music.
+
+To the rescue: This here.
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+// Taken from https://github.com/zmb3/spotify/blob/master/examples/authenticate/authcode/authenticate.go
+// Adjusted to own callback, and adding godotenv to load env vars directly from .env file
+
+// This example demonstrates how to authenticate with Spotify using the authorization code flow.
+// In order to run this example yourself, you'll need to:
+//
+//  1. Register an application at: https://developer.spotify.com/my-applications/
+//       - Use "http://localhost:8080/callback" as the redirect URI
+//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2/auth"
+
+	"github.com/zmb3/spotify/v2"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://127.0.0.1:58071/callback"
+
+var (
+	auth  *spotifyauth.Authenticator
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go func() {
+		err := http.ListenAndServe(":58071", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	auth = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}
